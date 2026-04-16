c188b4a2d5227989cf0ddddbcfb4bd018ab24512
Hitarth Khatiwala <hitarth1812@users.noreply.github.com>
2026-04-16 15:10:41 +0530

    Add --pythonpath flag to gunicorn to fix module import error

--- BACKEND/hems_backend/render-start.sh
+++ BACKEND/hems_backend/render-start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 cd BACKEND/hems_backend
-exec gunicorn hems_backend.wsgi:application --bind 0.0.0.0:$PORT --workers 2
+exec gunicorn --pythonpath . hems_backend.wsgi:application --bind 0.0.0.0:$PORT --workers 2
